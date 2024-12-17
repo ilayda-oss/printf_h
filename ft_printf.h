a7b0bbda2968d28c4b404d0fe5fc5c81a07c09ce
Sude Ilayda Özkara <suozkara@k1m25s06.42istanbul.com.tr>
2024-12-17 17:28:14 +0300

    son

--- ft_printf.h
+++ ft_printf.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printf.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sudeilaydaozkara <sudeilaydaozkara@stud    +#+  +:+       +#+        */
+/*   By: suozkara <suozkara@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/23 16:22:58 by suozkara          #+#    #+#             */
-/*   Updated: 2024/12/15 20:49:29 by sudeilaydao      ###   ########.fr       */
+/*   Updated: 2024/12/17 17:27:59 by suozkara         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ int	ft_putchar(char c);
 int	ft_putstr(char *s);
 int	ft_putnbr(int n);
 int	ft_putuns(unsigned int n);
-int	ft_puthex(unsigned long n, char c);
+int	ft_puthex(unsigned int n, char c);
 int	ft_putptr(unsigned long n);
 int	ft_printf(const char *s, ...);
 
